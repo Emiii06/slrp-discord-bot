eb6a0dd17a342f31462556640719e19d40d4cae1
Emily Kruse <emi@Mac.fritz.box>
2026-08-17 01:01:07 +0200

    more terms

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -10,7 +10,7 @@ const automod = require("../commands/automod");
 const continueApplication = require("../commands/continueapplication");
 const testApplication = require("../commands/testapplication");
 const tickets = require("../commands/tickets");
-const patchnotes = require("../commands/patchnote")
+const patchnote = require("../commands/patchnote")
 
 console.log("MESSAGE CREATE MODULE LOADED");
 
